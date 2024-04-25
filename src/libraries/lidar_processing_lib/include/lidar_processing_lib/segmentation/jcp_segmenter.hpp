6e61a96ce53bfef6f401693c00be43c909bc3df3
Yevgeniy <yevgeniy.engineer@gmail.com>
2024-04-25 14:15:58 +0000

    Setup ROS2 folder structure for lidar_processing_lib

---
+++ src/libraries/lidar_processing_lib/include/lidar_processing_lib/segmentation/jcp_segmenter.hpp
@@ -0,0 +1,19 @@
+// PCL
+#include <pcl/point_cloud.h> // pcl::PointCloud
+#include <pcl/point_types.h> // pcl::PointXYZI
+
+namespace lidar_perception_lib::segmentation
+{
+class JCPSegmenter
+{
+  public:
+    JCPSegmenter() = default;
+    ~JCPSegmenter() = default;
+
+    void segment()
+    {
+    }
+
+  private:
+};
+} // namespace lidar_perception_lib::segmentation
---
+++ src/libraries/lidar_processing_lib/src/segmentation/jcp_segmenter.cpp
@@ -0,0 +1,5 @@
+#include <lidar_processing_lib/segmentation/jcp_segmenter.hpp>
+
+namespace lidar_perception_lib::segmentation
+{
+}
