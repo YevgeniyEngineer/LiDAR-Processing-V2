a2076eb25906bae35532d40c9514f3bb30395ca5
Yevgeniy <yevgeniy.engineer@gmail.com>
2024-06-08 09:45:21 +0000

    Converting from Cartesian to Spherical, Building Hash Table

--- src/processor/src/clusterer.cpp
+++ src/processor/src/clusterer.cpp
@@ -24,4 +24,86 @@
 
 namespace clustering
 {
+Clusterer::Clusterer()
+{
+    spherical_cloud_.reserve(150'000);
+
+    voxel_grid_range_resolution_m_ = config_.voxel_grid_range_resolution_m_;
+    voxel_grid_azimuth_resolution_rad_ = config_.voxel_grid_azimuth_resolution_deg_ * DEG_TO_RAD;
+    voxel_grid_elevation_resolution_rad_ =
+        config_.voxel_grid_elevation_resolution_deg_ * DEG_TO_RAD;
 }
+
+void Clusterer::cluster(const pcl::PointCloud<pcl::PointXYZ>& cloud,
+                        std::vector<ClusterLabel>& labels)
+{
+}
+
+void Clusterer::cartesianToSpherical(const pcl::PointCloud<pcl::PointXYZ>& cartesian_cloud,
+                                     std::vector<SphericalPoint>& spherical_cloud)
+{
+    spherical_cloud.clear();
+    spherical_cloud.reserve(cartesian_cloud.size());
+
+    if (cartesian_cloud.empty())
+    {
+        return;
+    }
+
+    float max_range_m = std::numeric_limits<float>::lowest();
+    float max_azimuth_rad = std::numeric_limits<float>::lowest();
+    float max_elevation_rad = std::numeric_limits<float>::lowest();
+
+    for (const auto& cartesian_point : cartesian_cloud.points)
+    {
+        // Map Azimuth angle to 0 -> 2 * pi
+        float azimuth_rad = std::atan2(cartesian_point.y, cartesian_point.x);
+        azimuth_rad = (azimuth_rad < 0) ? (azimuth_rad + Clusterer::TWO_M_PIf) : azimuth_rad;
+        max_azimuth_rad = std::max(max_azimuth_rad, azimuth_rad);
+
+        // Range
+        const float distance_m_sqr =
+            cartesian_point.x * cartesian_point.x + cartesian_point.y * cartesian_point.y;
+        const float distance_m = std::sqrt(distance_m_sqr);
+        const float range_m = std::sqrt(distance_m_sqr + cartesian_point.z * cartesian_point.z);
+        max_range_m = std::max(max_range_m, range_m);
+
+        // Map Elevation angle to 0 -> pi
+        const float elevation_rad = std::atan(cartesian_point.z / distance_m) + M_PI_2f;
+        max_elevation_rad = std::max(max_elevation_rad, elevation_rad);
+
+        spherical_cloud.push_back({range_m, azimuth_rad, elevation_rad});
+    }
+
+    num_range_ =
+        static_cast<std::uint16_t>(std::ceil(max_range_m / voxel_grid_range_resolution_m_) + 1);
+
+    num_azimuth_ = static_cast<std::uint16_t>(
+        std::ceil(max_azimuth_rad / voxel_grid_azimuth_resolution_rad_) + 1);
+
+    num_elevation_ = static_cast<std::uint16_t>(
+        std::ceil(max_elevation_rad / voxel_grid_elevation_resolution_rad_) + 1);
+}
+
+void Clusterer::buildHashTable(
+    const std::vector<SphericalPoint>& spherical_cloud,
+    std::unordered_map<std::uint32_t, std::vector<std::uint32_t>>& hash_table)
+{
+    hash_table.clear();
+    hash_table.reserve(spherical_cloud.size());
+
+    for (std::uint32_t point_index = 0; point_index < spherical_cloud.size(); ++point_index)
+    {
+        const auto& point = spherical_cloud[point_index];
+
+        const std::uint16_t range_index = rangeToIndex(point.range_m);
+        const std::uint16_t azimuth_index = azimuthToIndex(point.azimuth_rad);
+        const std::uint16_t elevation_index = elevationToIndex(point.elevation_rad);
+        const std::uint32_t voxel_index =
+            flatVoxelIndex(range_index, azimuth_index, elevation_index);
+
+        hash_table_[voxel_index].push_back(point_index);
+    }
+}
+
+} // namespace clustering
--- src/processor/src/clusterer.hpp
+++ src/processor/src/clusterer.hpp
@@ -32,7 +32,9 @@
 #include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <limits>
 #include <ostream>
+#include <stdexcept>
 #include <utility>
 
 // PCL
@@ -44,12 +46,73 @@ namespace clustering
 
 using ClusterLabel = std::int32_t;
 
+struct SphericalPoint
+{
+    float range_m;
+    float azimuth_rad;   // Convention: 0 -> 2 * pi
+    float elevation_rad; // Convention: 0 -> pi
+};
+
+struct Configuration
+{
+    float voxel_grid_range_resolution_m_ = 1.0F;
+    float voxel_grid_azimuth_resolution_deg_ = 1.0F;
+    float voxel_grid_elevation_resolution_deg_ = 1.0F;
+};
+
 class Clusterer
 {
   public:
-    void cluster(pcl::PointCloud<pcl::PointXYZ>& cloud, std::vector<ClusterLabel>& labels);
+    static constexpr float TWO_M_PIf = static_cast<float>(2.0 * M_PI);
+    static constexpr float DEG_TO_RAD = static_cast<float>(M_PI / 180.0);
+
+    Clusterer();
+
+    void cluster(const pcl::PointCloud<pcl::PointXYZ>& cloud, std::vector<ClusterLabel>& labels);
 
   private:
+    Configuration config_;
+
+    float voxel_grid_range_resolution_m_;
+    float voxel_grid_azimuth_resolution_rad_;
+    float voxel_grid_elevation_resolution_rad_;
+
+    std::vector<SphericalPoint> spherical_cloud_;
+
+    std::uint16_t num_range_;
+    std::uint16_t num_azimuth_;
+    std::uint16_t num_elevation_;
+
+    std::unordered_map<std::uint32_t, std::vector<std::uint32_t>> hash_table_;
+
+    inline std::uint32_t flatVoxelIndex(std::uint16_t range_index,
+                                        std::uint16_t azimuth_index,
+                                        std::uint16_t elevation_index) const noexcept
+    {
+        return static_cast<std::uint32_t>(
+            num_range_ * (num_azimuth_ * elevation_index + azimuth_index) + range_index);
+    }
+
+    inline std::uint16_t rangeToIndex(float range_m) const noexcept
+    {
+        return static_cast<std::uint16_t>(range_m / voxel_grid_range_resolution_m_);
+    }
+
+    inline std::uint16_t azimuthToIndex(float azimuth_rad) const noexcept
+    {
+        return static_cast<std::uint16_t>(azimuth_rad / voxel_grid_azimuth_resolution_rad_);
+    }
+
+    inline std::uint16_t elevationToIndex(float elevation_rad) const noexcept
+    {
+        return static_cast<std::uint16_t>(elevation_rad / voxel_grid_elevation_resolution_rad_);
+    }
+
+    void cartesianToSpherical(const pcl::PointCloud<pcl::PointXYZ>& cartesian_cloud,
+                              std::vector<SphericalPoint>& spherical_cloud);
+
+    void buildHashTable(const std::vector<SphericalPoint>& spherical_cloud,
+                        std::unordered_map<std::uint32_t, std::vector<std::uint32_t>>& hash_table);
 };
 } // namespace clustering
 
