6ac492803bda0fdd7ae434711d2d887fcfce6802
Yevgeniy <yevgeniy.engineer@gmail.com>
2024-06-14 16:30:10 +0000

    Some refactoring to make processor cleaner

--- src/processor/src/processor.cpp
+++ src/processor/src/processor.cpp
@@ -20,152 +20,21 @@
  * SOFTWARE.
  */
 
+// Internal
+#include "processor.hpp"
+
 // STL
 #include <chrono>
-#include <cmath>
-#include <cstdint>
-#include <cstdlib>
 #include <cstring>
+#include <limits>
 #include <random>
 #include <tuple>
-#include <vector>
-
-// PCL
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 
 // ROS2
-#include <rclcpp/node.hpp>
 #include <rclcpp/qos.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp/subscription.hpp>
-#include <sensor_msgs/msg/image.hpp>
-#include <sensor_msgs/msg/point_cloud2.hpp>
-#include <sensor_msgs/msg/point_field.hpp>
-#include <visualization_msgs/msg/marker_array.hpp>
 
-// OpenCV
-#include <opencv2/opencv.hpp>
-
-// Segmenter
-#include "segmenter.hpp"
-
-// Clusterer
-#include "clusterer.hpp"
-
-// Polygonizer
-#include "polygonizer.hpp"
-
-class Node final : public rclcpp::Node
+namespace processor
 {
-  public:
-    using PointFieldTypes = pcl::PCLPointField::PointFieldTypes;
-    using PointCloud2 = sensor_msgs::msg::PointCloud2;
-    using MarkerArray = visualization_msgs::msg::MarkerArray;
-    using Image = sensor_msgs::msg::Image;
-
-    static constexpr std::size_t MAX_PTS = 200'000U;
-
-    Node() : rclcpp::Node{"processor"}
-    {
-        using std::placeholders::_1;
-
-        this->declare_parameter<std::string>("input_cloud_topic");
-        this->declare_parameter<std::string>("ground_cloud_topic");
-        this->declare_parameter<std::string>("obstacle_cloud_topic");
-        this->declare_parameter<std::string>("unsegmented_cloud_topic");
-        this->declare_parameter<std::string>("segmented_image_topic");
-        this->declare_parameter<std::string>("clustered_cloud_topic");
-        this->declare_parameter<std::string>("obstacle_outlines_topic");
-
-        input_cloud_topic_ = this->get_parameter("input_cloud_topic").as_string();
-        ground_cloud_topic_ = this->get_parameter("ground_cloud_topic").as_string();
-        obstacle_cloud_topic_ = this->get_parameter("obstacle_cloud_topic").as_string();
-        unsegmented_cloud_topic_ = this->get_parameter("unsegmented_cloud_topic").as_string();
-        segmented_image_topic_ = this->get_parameter("segmented_image_topic").as_string();
-        clustered_cloud_topic_ = this->get_parameter("clustered_cloud_topic").as_string();
-        obstacle_outlines_topic_ = this->get_parameter("obstacle_outlines_topic").as_string();
-
-        input_cloud_subscriber_ =
-            this->create_subscription<PointCloud2>(input_cloud_topic_,
-                                                   rclcpp::QoS(2).reliable().durability_volatile(),
-                                                   std::bind(&Node::topicCallback, this, _1));
-
-        ground_cloud_publisher_ = this->create_publisher<PointCloud2>(
-            ground_cloud_topic_, rclcpp::QoS(2).reliable().durability_volatile());
-        obstacle_cloud_publisher_ = this->create_publisher<PointCloud2>(
-            obstacle_cloud_topic_, rclcpp::QoS(2).reliable().durability_volatile());
-        unsegmented_cloud_publisher_ = this->create_publisher<PointCloud2>(
-            unsegmented_cloud_topic_, rclcpp::QoS(2).reliable().durability_volatile());
-        segmented_image_publisher_ = this->create_publisher<Image>(
-            segmented_image_topic_, rclcpp::QoS(2).reliable().durability_volatile());
-        clustered_cloud_publisher_ = this->create_publisher<PointCloud2>(
-            clustered_cloud_topic_, rclcpp::QoS(2).reliable().durability_volatile());
-        obstacle_outlines_publisher_ = this->create_publisher<MarkerArray>(
-            obstacle_outlines_topic_, rclcpp::QoS(2).reliable().durability_volatile());
-
-        // Reserve memory
-        input_cloud_.points.reserve(MAX_PTS);
-        ground_cloud_.points.reserve(MAX_PTS);
-        obstacle_cloud_.points.reserve(MAX_PTS);
-        unsegmented_cloud_.points.reserve(MAX_PTS);
-        image_cache_ = cv::Mat::zeros(
-            segmentation::Segmenter::IMAGE_HEIGHT, segmentation::Segmenter::IMAGE_WIDTH, CV_8UC3);
-        image_msg_cache_.data.reserve(3 * segmentation::Segmenter::IMAGE_HEIGHT *
-                                      segmentation::Segmenter::IMAGE_WIDTH);
-        cloud_msg_cache_.data.reserve(MAX_PTS);
-
-        polygonizer_indices_.reserve(MAX_PTS);
-        polygonizer_points_.reserve(MAX_PTS);
-        polygon_msg_cache_.markers.reserve(1000); // Max expected polygons
-        polygon_points_.reserve(100);             // Max points per polygon
-
-        RCLCPP_INFO(this->get_logger(), "%s node constructed", this->get_name());
-    }
-
-    void topicCallback(const PointCloud2& msg);
-
-  private:
-    std::string input_cloud_topic_;
-    std::string ground_cloud_topic_;
-    std::string obstacle_cloud_topic_;
-    std::string unsegmented_cloud_topic_;
-    std::string segmented_image_topic_;
-    std::string clustered_cloud_topic_;
-    std::string obstacle_outlines_topic_;
-
-    cv::Mat image_cache_;
-    Image image_msg_cache_;
-    PointCloud2 cloud_msg_cache_;
-    MarkerArray polygon_msg_cache_;
-
-    rclcpp::Subscription<PointCloud2>::SharedPtr input_cloud_subscriber_;
-    rclcpp::Publisher<PointCloud2>::SharedPtr ground_cloud_publisher_;
-    rclcpp::Publisher<PointCloud2>::SharedPtr obstacle_cloud_publisher_;
-    rclcpp::Publisher<PointCloud2>::SharedPtr unsegmented_cloud_publisher_;
-    rclcpp::Publisher<Image>::SharedPtr segmented_image_publisher_;
-    rclcpp::Publisher<PointCloud2>::SharedPtr clustered_cloud_publisher_;
-    rclcpp::Publisher<MarkerArray>::SharedPtr obstacle_outlines_publisher_;
-
-    pcl::PointCloud<pcl::PointXYZIR> input_cloud_;
-
-    segmentation::Segmenter segmenter_;
-    std::vector<segmentation::Label> segmentation_labels_;
-    pcl::PointCloud<pcl::PointXYZRGB> ground_cloud_;
-    pcl::PointCloud<pcl::PointXYZRGB> obstacle_cloud_;
-    pcl::PointCloud<pcl::PointXYZRGB> unsegmented_cloud_;
-
-    clustering::Clusterer clusterer_;
-    std::vector<clustering::ClusterLabel> clustering_labels_;
-    pcl::PointCloud<pcl::PointXYZRGB> clustered_cloud_;
-
-    polygonization::Polygonizer polygonizer_;
-
-    std::vector<std::uint32_t> polygonizer_indices_;
-    std::vector<polygonization::PointXY> polygonizer_points_;
-    std::vector<polygonization::PointXY> polygon_points_;
-};
-
 static void convertImageToRosMessage(const cv::Mat& cv_image,
                                      std::int32_t seconds,
                                      std::uint32_t nanoseconds,
@@ -188,7 +57,7 @@ static void convertImageToRosMessage(const cv::Mat& cv_image,
     // Copy image data
     const std::size_t size = msg.step * cv_image.rows;
     msg.data.resize(size);
-    std::memcpy(&msg.data[0], cv_image.data, size);
+    std::memcpy(static_cast<void*>(&msg.data[0]), static_cast<const void*>(cv_image.data), size);
 }
 
 static void convertPCLToPointCloud2(const pcl::PointCloud<pcl::PointXYZRGB>& cloud_pcl,
@@ -199,11 +68,11 @@ static void convertPCLToPointCloud2(const pcl::PointCloud<pcl::PointXYZRGB>& clo
     cloud_ros.width = cloud_pcl.width;
     cloud_ros.point_step = sizeof(pcl::PointXYZRGB);
 
-    std::vector<std::tuple<std::string, std::uint32_t, std::uint8_t, std::uint32_t>> fields = {
-        {"x", offsetof(pcl::PointXYZRGB, x), Node::PointFieldTypes::FLOAT32, 1},
-        {"y", offsetof(pcl::PointXYZRGB, y), Node::PointFieldTypes::FLOAT32, 1},
-        {"z", offsetof(pcl::PointXYZRGB, z), Node::PointFieldTypes::FLOAT32, 1},
-        {"rgb", offsetof(pcl::PointXYZRGB, rgb), Node::PointFieldTypes::FLOAT32, 1}};
+    static const std::vector<std::tuple<std::string, std::uint32_t, std::uint8_t, std::uint32_t>>
+        fields = {{"x", offsetof(pcl::PointXYZRGB, x), Processor::PointFieldTypes::FLOAT32, 1},
+                  {"y", offsetof(pcl::PointXYZRGB, y), Processor::PointFieldTypes::FLOAT32, 1},
+                  {"z", offsetof(pcl::PointXYZRGB, z), Processor::PointFieldTypes::FLOAT32, 1},
+                  {"rgb", offsetof(pcl::PointXYZRGB, rgb), Processor::PointFieldTypes::FLOAT32, 1}};
 
     for (const auto& field : fields)
     {
@@ -212,13 +81,18 @@ static void convertPCLToPointCloud2(const pcl::PointCloud<pcl::PointXYZRGB>& clo
         field_cache.offset = std::get<1>(field);
         field_cache.datatype = std::get<2>(field);
         field_cache.count = std::get<3>(field);
-        cloud_ros.fields.emplace_back(std::move(field_cache));
+        cloud_ros.fields.push_back(std::move(field_cache));
     }
 
-    const std::size_t byte_size = sizeof(pcl::PointXYZRGB) * cloud_pcl.size();
     cloud_ros.data.clear();
-    cloud_ros.data.resize(byte_size);
-    std::memcpy(cloud_ros.data.data(), &cloud_pcl.at(0), byte_size);
+    if (!cloud_pcl.empty())
+    {
+        const std::size_t byte_size = sizeof(pcl::PointXYZRGB) * cloud_pcl.size();
+        cloud_ros.data.resize(byte_size);
+        std::memcpy(static_cast<void*>(cloud_ros.data.data()),
+                    static_cast<const void*>(&cloud_pcl.at(0)),
+                    byte_size);
+    }
 };
 
 static void convertPolygonPointsToMarker(std::uint32_t polygon_index,
@@ -314,15 +188,72 @@ static void convertPolygonPointsToMarker(std::uint32_t polygon_index,
     }
 }
 
-void Node::topicCallback(const PointCloud2& msg)
+Processor::Processor() : rclcpp::Node{NODE_NAME}
+{
+    using std::placeholders::_1;
+
+    this->declare_parameter<std::string>("input_cloud_topic");
+    this->declare_parameter<std::string>("ground_cloud_topic");
+    this->declare_parameter<std::string>("obstacle_cloud_topic");
+    this->declare_parameter<std::string>("unsegmented_cloud_topic");
+    this->declare_parameter<std::string>("segmented_image_topic");
+    this->declare_parameter<std::string>("clustered_cloud_topic");
+    this->declare_parameter<std::string>("obstacle_outlines_topic");
+
+    input_cloud_topic_ = this->get_parameter("input_cloud_topic").as_string();
+    ground_cloud_topic_ = this->get_parameter("ground_cloud_topic").as_string();
+    obstacle_cloud_topic_ = this->get_parameter("obstacle_cloud_topic").as_string();
+    unsegmented_cloud_topic_ = this->get_parameter("unsegmented_cloud_topic").as_string();
+    segmented_image_topic_ = this->get_parameter("segmented_image_topic").as_string();
+    clustered_cloud_topic_ = this->get_parameter("clustered_cloud_topic").as_string();
+    obstacle_outlines_topic_ = this->get_parameter("obstacle_outlines_topic").as_string();
+
+    input_cloud_subscriber_ =
+        this->create_subscription<PointCloud2>(input_cloud_topic_,
+                                               rclcpp::QoS(2).reliable().durability_volatile(),
+                                               std::bind(&Processor::topicCallback, this, _1));
+
+    ground_cloud_publisher_ = this->create_publisher<PointCloud2>(
+        ground_cloud_topic_, rclcpp::QoS(2).reliable().durability_volatile());
+    obstacle_cloud_publisher_ = this->create_publisher<PointCloud2>(
+        obstacle_cloud_topic_, rclcpp::QoS(2).reliable().durability_volatile());
+    unsegmented_cloud_publisher_ = this->create_publisher<PointCloud2>(
+        unsegmented_cloud_topic_, rclcpp::QoS(2).reliable().durability_volatile());
+    segmented_image_publisher_ = this->create_publisher<Image>(
+        segmented_image_topic_, rclcpp::QoS(2).reliable().durability_volatile());
+    clustered_cloud_publisher_ = this->create_publisher<PointCloud2>(
+        clustered_cloud_topic_, rclcpp::QoS(2).reliable().durability_volatile());
+    obstacle_outlines_publisher_ = this->create_publisher<MarkerArray>(
+        obstacle_outlines_topic_, rclcpp::QoS(2).reliable().durability_volatile());
+
+    // Reserve memory
+    input_cloud_.points.reserve(MAX_PTS);
+    ground_cloud_.points.reserve(MAX_PTS);
+    obstacle_cloud_.points.reserve(MAX_PTS);
+    unsegmented_cloud_.points.reserve(MAX_PTS);
+    image_cache_ = cv::Mat::zeros(
+        segmentation::Segmenter::IMAGE_HEIGHT, segmentation::Segmenter::IMAGE_WIDTH, CV_8UC3);
+    image_msg_cache_.data.reserve(3 * segmentation::Segmenter::IMAGE_HEIGHT *
+                                  segmentation::Segmenter::IMAGE_WIDTH);
+    cloud_msg_cache_.data.reserve(MAX_PTS);
+
+    polygonizer_indices_.reserve(MAX_PTS);
+    polygonizer_points_.reserve(MAX_PTS);
+    polygon_msg_cache_.markers.reserve(1000); // Max expected polygons
+    polygon_points_.reserve(100);             // Max points per polygon
+
+    RCLCPP_INFO(this->get_logger(), "%s node constructed", this->get_name());
+}
+
+void Processor::topicCallback(const PointCloud2& msg)
 {
     // Convert point cloud from sensor_msgs::msg::PointCloud2 to pcl::PointCloud<pcl::PointXYZIR>
     input_cloud_.clear();
     input_cloud_.header.frame_id = msg.header.frame_id;
     input_cloud_.width = msg.width;
     input_cloud_.height = msg.height;
-    input_cloud_.header.stamp = static_cast<std::uint64_t>(
-        std::round(msg.header.stamp.sec * 1e9 + msg.header.stamp.nanosec) / 1e3);
+    input_cloud_.header.stamp = static_cast<std::uint64_t>(static_cast<std::uint64_t>(
+        std::round(msg.header.stamp.sec * 1.0e9 + msg.header.stamp.nanosec) / 1.0e3));
 
     input_cloud_.points.resize(msg.height * msg.width);
     const auto point_step = msg.point_step;
@@ -330,10 +261,11 @@ void Node::topicCallback(const PointCloud2& msg)
 
     for (std::uint32_t row = 0; row < msg.height; ++row)
     {
+        const auto base_row_offset = row * row_step;
         for (std::uint32_t col = 0; col < msg.width; ++col)
         {
             auto& point = input_cloud_.points[row * msg.width + col];
-            const auto offset = row * row_step + col * point_step;
+            const auto offset = base_row_offset + col * point_step;
 
             point.x = *reinterpret_cast<const float*>(&msg.data[offset + msg.fields[0].offset]);
             point.y = *reinterpret_cast<const float*>(&msg.data[offset + msg.fields[1].offset]);
@@ -361,15 +293,15 @@ void Node::topicCallback(const PointCloud2& msg)
 
         if (label == segmentation::Label::GROUND)
         {
-            ground_cloud_.push_back({p.x, p.y, p.z, 124, 252, 0});
+            ground_cloud_.emplace_back(p.x, p.y, p.z, 124, 252, 0);
         }
         else if (label == segmentation::Label::OBSTACLE)
         {
-            obstacle_cloud_.push_back({p.x, p.y, p.z, 200, 0, 0});
+            obstacle_cloud_.emplace_back(p.x, p.y, p.z, 200, 0, 0);
         }
         else
         {
-            unsegmented_cloud_.push_back({p.x, p.y, p.z, 255, 255, 0});
+            unsegmented_cloud_.emplace_back(p.x, p.y, p.z, 255, 255, 0);
         }
     }
 
@@ -434,8 +366,8 @@ void Node::topicCallback(const PointCloud2& msg)
                 if (clustering_labels_[point_index] == label)
                 {
                     const auto& point = obstacle_cloud_[point_index];
-                    clustered_cloud_.push_back({point.x, point.y, point.z, r, g, b});
 
+                    clustered_cloud_.emplace_back(point.x, point.y, point.z, r, g, b);
                     polygonizer_points_.push_back({point.x, point.y});
 
                     if (point.z < z_min)
@@ -546,6 +478,7 @@ void Node::topicCallback(const PointCloud2& msg)
         obstacle_outlines_publisher_->publish(polygon_msg_cache_);
     }
 }
+} // namespace processor
 
 std::int32_t main(std::int32_t argc, const char* const* argv)
 {
@@ -556,7 +489,7 @@ std::int32_t main(std::int32_t argc, const char* const* argv)
 
     try
     {
-        rclcpp::spin(std::make_shared<Node>());
+        rclcpp::spin(std::make_shared<processor::Processor>());
     }
     catch (const std::exception& e)
     {
---
+++ src/processor/src/processor.hpp
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2024 Yevgeniy Simonov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef PROCESSOR_HPP
+#define PROCESSOR_HPP
+
+// Segmenter
+#include "segmenter.hpp"
+
+// Clusterer
+#include "clusterer.hpp"
+
+// Polygonizer
+#include "polygonizer.hpp"
+
+// STL
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+// PCL
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+// ROS2
+#include <rclcpp/node.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/subscription.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/point_field.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
+
+// OpenCV
+#include <opencv2/opencv.hpp>
+
+namespace processor
+{
+class Processor final : public rclcpp::Node
+{
+  public:
+    static constexpr const char* NODE_NAME = "processor";
+    static constexpr std::size_t MAX_PTS = 200'000U;
+
+    using PointFieldTypes = pcl::PCLPointField::PointFieldTypes;
+    using PointCloud2 = sensor_msgs::msg::PointCloud2;
+    using MarkerArray = visualization_msgs::msg::MarkerArray;
+    using Image = sensor_msgs::msg::Image;
+
+    Processor();
+
+    void topicCallback(const PointCloud2& msg);
+
+  private:
+    std::string input_cloud_topic_;
+    std::string ground_cloud_topic_;
+    std::string obstacle_cloud_topic_;
+    std::string unsegmented_cloud_topic_;
+    std::string segmented_image_topic_;
+    std::string clustered_cloud_topic_;
+    std::string obstacle_outlines_topic_;
+
+    cv::Mat image_cache_;
+    Image image_msg_cache_;
+    PointCloud2 cloud_msg_cache_;
+    MarkerArray polygon_msg_cache_;
+
+    rclcpp::Subscription<PointCloud2>::SharedPtr input_cloud_subscriber_;
+    rclcpp::Publisher<PointCloud2>::SharedPtr ground_cloud_publisher_;
+    rclcpp::Publisher<PointCloud2>::SharedPtr obstacle_cloud_publisher_;
+    rclcpp::Publisher<PointCloud2>::SharedPtr unsegmented_cloud_publisher_;
+    rclcpp::Publisher<Image>::SharedPtr segmented_image_publisher_;
+    rclcpp::Publisher<PointCloud2>::SharedPtr clustered_cloud_publisher_;
+    rclcpp::Publisher<MarkerArray>::SharedPtr obstacle_outlines_publisher_;
+
+    pcl::PointCloud<pcl::PointXYZIR> input_cloud_;
+
+    segmentation::Segmenter segmenter_;
+    std::vector<segmentation::Label> segmentation_labels_;
+    pcl::PointCloud<pcl::PointXYZRGB> ground_cloud_;
+    pcl::PointCloud<pcl::PointXYZRGB> obstacle_cloud_;
+    pcl::PointCloud<pcl::PointXYZRGB> unsegmented_cloud_;
+
+    clustering::Clusterer clusterer_;
+    std::vector<clustering::ClusterLabel> clustering_labels_;
+    pcl::PointCloud<pcl::PointXYZRGB> clustered_cloud_;
+
+    polygonization::Polygonizer polygonizer_;
+
+    std::vector<std::uint32_t> polygonizer_indices_;
+    std::vector<polygonization::PointXY> polygonizer_points_;
+    std::vector<polygonization::PointXY> polygon_points_;
+};
+
+} // namespace processor
+
+#endif // PROCESSOR_HPP
